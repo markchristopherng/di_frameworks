5dedf6594b8cd3954ccadc32dc968cdfc37a4b60
ngm <mark.ng@auspost.com.au>
2020-02-21 10:59:43 +1100

    simplify message data

--- Dagger/app/src/main/java/au/com/auspost/android/feature/dagger/GreetingServiceImpl.kt
+++ Dagger/app/src/main/java/au/com/auspost/android/feature/dagger/GreetingServiceImpl.kt
@@ -3,14 +3,7 @@ package au.com.auspost.android.feature.dagger
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class MessageData {
-
-    var welcomeMessage: String = ""
-
-    @Inject constructor(welcomeMessage: String) {
-        this.welcomeMessage = welcomeMessage
-    }
-}
+class MessageData  @Inject constructor(val welcomeMessage: String)
 
 @Singleton
 class GreetingServiceImpl @Inject constructor(private val messageData: MessageData, private val timeService: TimeService) :
